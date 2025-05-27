06af99acc0bea3912e3e10c794f1fbf5cca2e66a
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-05-26 22:36:50 -0400

    Update solution.py

--- problems/solutions/0009-palindrome-number/solution.py
+++ problems/solutions/0009-palindrome-number/solution.py
@@ -1,3 +1,7 @@
-## class Solution:
-##     def isPalindrome(self, x: int) -> bool:
-##         
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        reverso, numero = 0, x
+        while x>0:
+            reverso = reverso*10+x%10
+            x//=10
+        return True if reverso==numero else False
